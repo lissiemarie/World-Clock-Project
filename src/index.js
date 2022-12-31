62380455a43641f84ee92a99b05a6fee54aa4929
lissiemarie <elyssamunden@gmail.com>
2022-12-31 09:25:28 -0800

    movede JS into HTML, fixed JS

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const getCityTime = event => {
-    if(event.target.length > 0){
-        let time = moment().tz(event.target.value).format('dddd, MMMM D, YYYY h:mm A');
-
-        alert(`It is ${time} in ${event.target.value}`);
-    }
-};
-
-document.querySelector("#countries").addEventListener("change", getCityTime);
