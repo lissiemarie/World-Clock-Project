403bafc0055d78fb769482a66036fbd532ec0eb1
lissiemarie <elyssamunden@gmail.com>
2023-01-01 15:41:22 -0800

    added querySelectors for loaded times

--- src/index.js
+++ src/index.js
@@ -10,3 +10,19 @@
 
       let element = document.querySelector("#countries");
       element.addEventListener("change", getCityTime);
+
+    //   New York Elements
+      let newYorkElement = document.querySelector("#new-york");
+      let newYorkDateElement = document.querySelector("#new-york .date");
+      let newYorkTime = document.querySelector("#new-york-time");
+
+    //   Berlin Elements
+    let berlinElement = document.querySelector("#berlin");
+      let berlinDateElement = document.querySelector("#berlin .date");
+      let berlinTime = document.querySelector("#berlin-time");
+
+    //Aukland Elements
+    let auklandElement = document.querySelector("#aukland");
+      let auklandDateElement = document.querySelector("#aukland .date");
+      let auklandTime = document.querySelector("#aukland-time");
+      
